efeffdd87800e3fc3e705cf2539cde77d01bcf7f
rraina45 <rainarohan1234@gmail.com>
2024-12-10 21:15:54 -0500

    Update App.test.js

--- rent-hub-fe/src/App.test.js
+++ rent-hub-fe/src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App.js';
 
 test('renders learn react link', () => {
-  render(<App.js />);
+  render(<App/>);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
